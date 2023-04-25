d2fb6c8d7431891433816ce95726c22e5a3744f1
Abdoul08 <diawarabdoulaye55@gmail.com>
2023-04-25 20:29:13 +0200

    jointure active record

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -6,10 +6,9 @@ class ExercisesController < ApplicationController
   end
 
   def exercise2
-    @shops = Shop
-      .left_outer_joins(foods: :order_foods)
-      .distinct.where(foods: { order_foods: { id: nil } })
-
+    # 【要件】注文されていない料理を提供しているすべてのお店を返すこと
+    #   * left_outer_joinsを使うこと
+    @shops = Shop.left_outer_joins(foods: :orders).distinct.where(foods: { orders: { id: nil } })
   end
 
   def exercise3 
